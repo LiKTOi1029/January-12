e5670c45b285c1a869a0a4255e542e9ef8bd6dd6
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-12 14:13:24 +0330

    Making a string reverser

---
+++ StringReverser.lua
@@ -0,0 +1,13 @@
+wordList = {}
+setString = ""
+userInput = io.read()
+for num0 = 1, string.len(userInput), 1 do
+	if string.sub(userInput, num0, num0) ~= " " then
+		local putTogetherString = string.sub(userInput,num0,num0)
+		setString = setString..putTogetherString
+	elseif string.sub(userInput, num0, num0) == " " or num0 == string.len(userInput) then
+		print(num0..setString.." "..string.len(userInput))
+		table.insert(wordList, setString)
+		setString = ""
+	end
+end
