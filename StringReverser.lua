8148d20593e13bc5d9dedaef64204ac63441272f
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-12 14:26:55 +0330

    finishing up coding challenge StringReverser.lua

--- StringReverser.lua
+++ StringReverser.lua
@@ -1,14 +1,24 @@
 wordList = {}
 setString = ""
 userInput = io.read()
+function reverse(Table)
+    for num1 = 1, math.floor(#Table / 2) do
+        Table[num1], Table[#Table - num1 + 1] = Table[#Table - num1 + 1], Table[num1]
+    end
+    return Table
+end
 for num0 = 1, string.len(userInput), 1 do
 	if string.sub(userInput, num0, num0) ~= " " then
 		local putTogetherString = string.sub(userInput,num0,num0)
 		setString = setString..putTogetherString
 	end
 	if string.sub(userInput, num0, num0) == " " or num0 == string.len(userInput) then
-		print(num0..setString.." "..string.len(userInput))
 		table.insert(wordList, setString)
 		setString = ""
 	end
 end
+reverse(wordList)
+for num2 = 1, #wordList, 1 do
+	setString = setString.." "..wordList[num2]
+end
+print(setString)
