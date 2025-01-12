d2c97fe6def850b7362c8d1acfe4579bd06edae9
LiKTOi1029 <testingboy5689@gmail.com>
2025-01-12 14:16:51 +0330

    adding in function to separate and add individual words to a table StringReverser.lua

--- StringReverser.lua
+++ StringReverser.lua
@@ -5,7 +5,8 @@ for num0 = 1, string.len(userInput), 1 do
 	if string.sub(userInput, num0, num0) ~= " " then
 		local putTogetherString = string.sub(userInput,num0,num0)
 		setString = setString..putTogetherString
-	elseif string.sub(userInput, num0, num0) == " " or num0 == string.len(userInput) then
+	end
+	if string.sub(userInput, num0, num0) == " " or num0 == string.len(userInput) then
 		print(num0..setString.." "..string.len(userInput))
 		table.insert(wordList, setString)
 		setString = ""
